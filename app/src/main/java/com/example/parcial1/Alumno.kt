d75bb17dbf8644f5b9bfcda5836a4d03f7dbffb3
Carolina <carodojcius@gmail.com>
2023-05-17 21:53:36 -0300

    Se agrega la clase Alumno

---
+++ app/src/main/java/com/example/parcial1/Alumno.kt
@@ -0,0 +1,8 @@
+package com.example.parcial1
+
+data class Alumno(
+    val id: Int,
+    val name: String,
+    val edad: Int,
+    val url: String
+)
--- app/src/main/java/com/example/parcial1/MainActivity.kt
+++ app/src/main/java/com/example/parcial1/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.parcial1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var botonLogin: Button
+    private lateinit var textVNombre: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        botonLogin = findViewById(R.id.buttonLogin)
+        textVNombre= findViewById(R.id.textViewNombre)
     }
+
+private fun getListadoAlumnos(): MutableList<Alumno>?{
+    return mutableListOf(
+        Alumno(1, "Jim",20,"https://roost.nbcuni.com/bin/viewasset.html/content/dam/Peacock/Landing-Pages/2-0-design/the-office/cast-the-office-jim-halpert.jpg/_jcr_content/renditions/original.JPEG"),
+        Alumno(1, "Jim",20,"https://roost.nbcuni.com/bin/viewasset.html/content/dam/Peacock/Landing-Pages/2-0-design/the-office/cast-the-office-jim-halpert.jpg/_jcr_content/renditions/original.JPEG"),
+        Alumno(1, "Jim",20,"https://roost.nbcuni.com/bin/viewasset.html/content/dam/Peacock/Landing-Pages/2-0-design/the-office/cast-the-office-jim-halpert.jpg/_jcr_content/renditions/original.JPEG"),
+        Alumno(1, "Jim",20,"https://roost.nbcuni.com/bin/viewasset.html/content/dam/Peacock/Landing-Pages/2-0-design/the-office/cast-the-office-jim-halpert.jpg/_jcr_content/renditions/original.JPEG"),
+        Alumno(1, "Jim",20,"https://roost.nbcuni.com/bin/viewasset.html/content/dam/Peacock/Landing-Pages/2-0-design/the-office/cast-the-office-jim-halpert.jpg/_jcr_content/renditions/original.JPEG"),
+        Alumno(1, "Jim",20,"https://roost.nbcuni.com/bin/viewasset.html/content/dam/Peacock/Landing-Pages/2-0-design/the-office/cast-the-office-jim-halpert.jpg/_jcr_content/renditions/original.JPEG"),
+        Alumno(1, "Jim",20,"https://roost.nbcuni.com/bin/viewasset.html/content/dam/Peacock/Landing-Pages/2-0-design/the-office/cast-the-office-jim-halpert.jpg/_jcr_content/renditions/original.JPEG"),
+        Alumno(1, "Jim",20,"https://roost.nbcuni.com/bin/viewasset.html/content/dam/Peacock/Landing-Pages/2-0-design/the-office/cast-the-office-jim-halpert.jpg/_jcr_content/renditions/original.JPEG"),
+        Alumno(1, "Jim",20,"https://roost.nbcuni.com/bin/viewasset.html/content/dam/Peacock/Landing-Pages/2-0-design/the-office/cast-the-office-jim-halpert.jpg/_jcr_content/renditions/original.JPEG"),
+        Alumno(1, "Jim",20,"https://roost.nbcuni.com/bin/viewasset.html/content/dam/Peacock/Landing-Pages/2-0-design/the-office/cast-the-office-jim-halpert.jpg/_jcr_content/renditions/original.JPEG")
+    )
+}
 }
